012c8a950ead27571d55eca8efb2d44c14437690
APPLE DEVELOPER ACADEMY <appledeveloperacademy@10-42-40-59.0101.pcpr.br.iosda.org>
2019-03-23 09:23:06 -0300

    fix pages

--- WWDC_Enzo.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page1.playgroundpage/Contents.swift
+++ WWDC_Enzo.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page1.playgroundpage/Contents.swift
@@ -32,36 +32,6 @@
  
  */
 
-
-
-//#-hidden-code
-
-import PlaygroundSupport
-import SpriteKit
-import Foundation
-// Load the SKScene from 'GameScene.sks'
-let sceneView = SKView(frame: CGRect(x:0 , y:0, width: SCREEN_WIDTH, height: SCREEN_HEIGHT))
-if let scene = GameScene(fileNamed: "GameScene") {
-    // Set the scale mode to scale to fit the window
-    scene.backgroundColor = ICE_WHITE
-    scene.scaleMode = .aspectFill
-    
-    // Present the scene
-    sceneView.presentScene(scene)
-    //sceneView.showsPhysics = true
-    sceneView.showsFPS = true
-    sceneView.isMultipleTouchEnabled = true
-    //sceneView.showsFields = true
-    
-    // Load the SKScene from 'GameScene.sks'
-    
-}
-
-PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
-
-//#-end-hidden-code
-
-
 //#-hidden-code
 
 import PlaygroundSupport
